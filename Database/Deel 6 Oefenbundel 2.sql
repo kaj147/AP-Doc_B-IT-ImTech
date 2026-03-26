0a022074eafc9d31e9511b29a61b7bfb3ac28219
kaj147 <chr.vdhdn@gmail.com>
2026-03-26 11:46:20 +0100

    reset

--- Database/Deel 6 Oefenbundel 2.sql
+++
@@ -1,8 +0,0 @@
--- Toon alle persoonsgegevens van de voorzitters en penningmeesters. Dit omvat alle info die in tabel spelers zit van deze personen, de functie en een 
--- indicatie “Actief” of “Niet actief” afhankelijk van het al dan niet ingevuld zijn van de einddatum
-
-SELECT
-	`S`.*
-FROM 
-	`spelers` AS `S`
-    INNER JOIN `bestuursleden` AS `B` ON `B`.`SPELERS` = `S`.`SPELERSNR`
